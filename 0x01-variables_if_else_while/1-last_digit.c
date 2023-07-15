f41d4ed57650fb18e0a037108706cffcb6475a6a
Oear12 <oceancleanup38@gmail.com>
2023-07-15 16:19:45 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is greater than 6 is less than  0\n", n, ld);
+		printf("Last digit of %d is %d and is less than 6 and not  0\n", n, ld);
 	}
 	return (0);
 }
