011e0b1fa1cd03c4577c28e40c2aa0006290bc71
Oear12 <oceancleanup38@gmail.com>
2023-07-15 17:17:01 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	int n =97;
-	int m =65;
+	int n = 97;
+	int m = 65;
 
 	while (n <= 122)
 	{
@@ -21,7 +21,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
-
-	
-			
+}			
