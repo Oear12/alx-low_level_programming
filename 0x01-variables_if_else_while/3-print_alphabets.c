0a478e5f884a5caa82513258fbd6bf9fa47fd258
Oear12 <oceancleanup38@gmail.com>
2023-07-15 17:04:40 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Awlays 0
+ */
+int main(void)
+{
+	int n =97;
+	int m =65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		n++;
+	}
+	putchar('\n');
+	return(0);
+}
+
+	
+			
