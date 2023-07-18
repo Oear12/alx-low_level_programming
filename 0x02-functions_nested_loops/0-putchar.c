c13e38607758390e28d57a7c0359d549df1d2bf1
Oear12 <oceancleanup38@gmail.com>
2023-07-18 11:09:28 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * main - Entry point 
+ * main - Entry point
+ *
  * Return: always 0
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
