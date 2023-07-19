0efddfdc742ddb6409ccfede91a2582c8229e3f8
Oear12 <oceancleanup38@gmail.com>
2023-07-19 05:43:35 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-islower.c -o 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _islower - checks for charcherif input in is a
- * lowercase char. another case, shows
- * 0
- *
- *
+ * _islower - checks for lowercase character
+ * @c: The character to be checked
  * Return: 1 for lowercase char. 0 for the rest,
  */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
