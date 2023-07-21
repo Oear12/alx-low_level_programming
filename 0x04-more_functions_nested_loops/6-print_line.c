f0cef8046bc143ba0c6dffc8ffeb4199c6f95f42
Oear12 <oceancleanup38@gmail.com>
2023-07-20 20:37:14 -0700

    c fun

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times character _ should be printed
+ */
+voidprint_line(int n)
+{
+	if (n <= 0)
+	{
+		_pitchar('\n');
+	} else
+	{
+		int i;
+
+		_putchar(i = 1; i <= n; i++)
+		{
+			_pitchar('_');
+		}
+
+		_putchar('\n');
+	}
+}
+			
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a squre, folled by a new line
+ * @size:size of the square
+ */
+void print_square(int size)
+{
+	if (size <= o)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i =0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
