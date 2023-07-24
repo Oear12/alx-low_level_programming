4511184c3a03e7aa59bc0caa506af0011e665a04
Oear12 <oceancleanup38@gmail.com>
2023-07-23 17:45:05 -0700

    gggagdg

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
-
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - prints an integer
@@ -12,7 +12,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		_putchar('-');
+		putchar('-');
 	} else
 	{
 		n1 = n;
@@ -23,5 +23,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 
-	_putchar((n1 % 10) + '0');
+	putchar((n1 % 10) + '0');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,20 @@
 #include "main.h"
-#include <string.h>
 #include <stdio.h>
+
 /**
- * print_most_numbers - function checks for checks for a digit  (0 to 9)
+ * print_most_numbers - print num except 2 and 4
  *
- * Return: always 0
+ * return: void
  */
 
 void print_most_numbers(void)
 {
-int c;
-for (c = 48; c < 58; c++)
-{
-if (c != 50)
-{
-if (c != 52)
-{
-putchar(c = '0');
-}
-}
-}
-putchar('\n');
+        char c;
+
+        for (c = '0'; c <= '9'; c++)
+        {
+                if (!(c == '2' || c == '4'))
+                        putchar(c);
+        }
+        putchar('\n');
 }
